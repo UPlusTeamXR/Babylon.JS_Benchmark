768cde634335172f497da045fade030c0503aaf4
hanquel85 <89120890+hanquel85@users.noreply.github.com>
2023-03-07 22:34:55 +0900

    feat : preview babylon.js 추가, UI js 추가

---
+++ scripts/benchmark.js
@@ -0,0 +1,12 @@
+const getFrameRate = function () {
+    // console.log("getFrameRate");
+
+    return 0;
+}
+
+const UpdateUI = function (scene) {
+    getFrameRate();
+    // console.log("UpdateUI");
+
+    return null;
+}
